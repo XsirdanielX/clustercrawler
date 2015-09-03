f051f98469e37541b0c8da3c78488b62e0a8d070
Alex <alexander.platz@campus.tu-berlin.de>
2015-09-03 16:06:28 +0200

    Connection-Retry function implemented

--- src/ncbiHttpClient_stdin.py
+++ src/ncbiHttpClient_stdin.py
@@ -1,28 +1,59 @@
+#!/usr/bin/env python
+
 # -----------------------------------------------------------------------
 # --- This is a standalone client to download automatically big data sets
 # --- from the ncbi database in fasta format. This script provides the
 # --- functionality to receive search requests via command line input
 # --- from a user.
+# ---
 # ---                        @author: alexander.platz@campus.tu-berlin.de
 # ---                        @date:   02. Sep 2015
 # -----------------------------------------------------------------------
 # --- Usage: >python ncbiHttpClient_stdin.py
 # -----------------------------------------------------------------------
+
+
+
+from urllib2 import Request, urlopen, URLError, HTTPError
 import urllib2
 import re
+import time
 from lxml import etree
 from io import StringIO, BytesIO
 
-bpLinearDNA = 3800000
+bpLinearDNA = 380000
 retMax = '100'
 retStart = '0'
 
 # --- HTTP request - response to get XML
 def sendRequest(url):
 	req = urllib2.Request(url)
-	response = urllib2.urlopen(req)
+	retries = 1
+	established = False
+	while not established:
+		try:
+			response = urllib2.urlopen(req)
+			established = True
+		except HTTPError as e:
+			print 'The server couldn\'t handle the request.'
+			print 'Error code: ', e.code
+			waiting(retries)
+			retries += 1
+		except URLError as e:
+			print 'Failed to reach a server.'
+			print 'Reason: ', e.reason
+			waiting(retries)
+			retries += 1
 	page = response.read()
 	return page
+
+def waiting(retries):
+	print "%s. retry attempt..." %retries
+	wait = retries * 2
+	if wait > 60:
+		wait = 60
+	time.sleep(wait)
+
 
 def esearchUrlBuilder(retStart, searchString):
 	str(retStart)
@@ -32,12 +63,12 @@ def esearchUrlBuilder(retStart, searchString):
 
 def esummaryUrlBuilder(fragmentedArray):
 	url_esummary = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=nuccore&id='
-	url_esummary += ','.join(fragmentedArray) #appending transformed string to url
+	url_esummary += ','.join(fragmentedArray) #appending ids to the URL 
 	return url_esummary
 
 def efetchUrlBuilder(fragmentedArray):
 	url_efetch = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&rettype=fasta&retmode=text&id='
-	url_efetch += ','.join(fragmentedArray)
+	url_efetch += ','.join(fragmentedArray) #appending ids to the URL
 	return url_efetch
 
 def processUserInput():
@@ -54,7 +85,7 @@ with open(searchString+'.fasta', 'wb') as f:
 	f.write('')
 
 elementArray = []
-# --- get all the IDs/Gis for the search string
+# --- get all the IDs/Gis of the search string
 while True:
 	url_esearch = esearchUrlBuilder(retStart, searchString)
 	xmlroot = etree.XML(sendRequest(url_esearch)) #.XML() returns root node, .fromstring() as well
@@ -75,6 +106,7 @@ while True:
 begin = 0
 until = retMax
 fragmentedArray = []
+# --- 
 while True:
 	print 'range: %d' %(len(elementArray))
 	for i in range(begin, until):
@@ -91,8 +123,8 @@ while True:
 	url_esummary = esummaryUrlBuilder(fragmentedArray)
 	xmlroot = etree.XML(sendRequest(url_esummary))
 
-	# --- searching for 'bp linear DNA' values
-	# --- in xml output it's attribute 'Length'
+	# --- searching for 'bp linear DNA' values. In xml output it is the attribute 'Length'
+	# --- only take the base pairs greater than the value assigned to 'bpLinearDNA'.
 	del fragmentedArray[:]
 	for xmlchilds in xmlroot.iter('Item'):
 		if xmlchilds.get('Name') == 'Gi':
