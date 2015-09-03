7dc2694df0b6af6c10f343f6880a39c39d6bd96f
Alex <alexander.platz@campus.tu-berlin.de>
2015-09-03 17:40:43 +0200

    starting script for connecting ncbi downloading and antismash processing

--- src/runGenCrawler.sh
+++ src/runGenCrawler.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
-python ncbiHttpClient.py
-run_antismash sequence.fasta ../out/ 
+# --- Checking whether an argument passed
+if [ $# -ne 1 ]; then
+	echo ""
+    echo "$0 - Usage: ./runGenCrawler <search term>"
+    echo "As a whitespace please use '+'"
+    exit 1
+fi
+
+name=$1 # assigns value from argv[1] in variable 'name'
+
+echo "Starting NCBI download..."
+python ncbiHttpClient.py $name
+echo "NCBI download completed..."
+
+fastaFiles=(Xant*.fasta)
+echo "Number of fasta files found: ${#fastaFiles[@]}"
+
+echo "Starting Antismash..."
+for i in `seq 0 ${#fastaFiles[@]}`
+do
+	run_antismash ${fastaFiles[i]} ../out/
+done
+echo "Antismash completed"
