d09f3de17591a1f8c9d82be747768c5fa219d1ad
Alex <alexander.platz@campus.tu-berlin.de>
2015-09-21 11:34:11 +0200

    working on Socket.io and zeroRPC

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var zerorpc = require("zerorpc")
+
 var connectionCount = 0;
+
+http.listen(3333, function(){
+	console.log('genCrawler web interface listening on port :3333');
+});
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
@@ -12,13 +18,30 @@ app.get('/test', function(req, res){
 	res.send('<h1>TEST Hello world TEST</h1>');
 });
 
-io.on('connect', function(client){
+io.on('connect', function(client) {
 	connectionCount++;
+
+
+
+	var c = new zerorpc.Client();
+	c.connect("tcp://127.0.0.1:4242");
+
+	c.invoke("hello", "RPC", function(error, res, more) {
+    	console.log(res);
+	});
+
+
+
+
+
+
+
 	console.log('new job, '+connectionCount+' jobs are running.');
-	client.on('disconnect', function(){
+	client.on('disconnect', function() {
 		connectionCount--;
 		console.log('Job aborted, '+connectionCount+' remaining jobs are running');
 	});
+
 	// --- join
 	client.on('join', function(data) {
 		client.emit('msg', 'Hello from genCrawler server');
@@ -30,8 +53,4 @@ io.on('connect', function(client){
 		client.emit('msg', 'Echo: ' +data);
 		
 	});
-});
-
-http.listen(3333, function(){
-	console.log('genCrawler web interface listening on port :3333');
 });
---
+++ src/zerorpcClient.py
@@ -0,0 +1,7 @@
+#!\Python27\python.exe
+
+import zerorpc
+
+c = zerorpc.Client()
+c.connect("tcp://127.0.0.1:4242")
+print c.hello("RPC")
